207ba6a5cb18350269dc6685ddfd3aac70ee08bd
Tandoan19 <tandoan.hcmut@gmail.com>
2023-11-28 21:53:00 +0700

    add comments

--- omh_compute/omh.cpp
+++ omh_compute/omh.cpp
@@ -16,15 +16,15 @@ char comp(char c) {
   return c;
 }
 
-std::string reverse_complement(const std::string& sequence) {
-  std::string res(sequence.size(), '\0');
+std::string reverse_complement(const std::string& sequence) { // return reverse complement of a sequence
+  std::string res(sequence.size(), '\0'); // creates a string res of the same size as sequence, initially filled with null characters
 
-  auto sf = sequence.begin();
+  auto sf = sequence.begin(); // create iterators pointing to the start and end of sequence and res
   auto sl = sequence.end() - 1;
   auto rf = res.begin();
   auto rl = res.end() - 1;
 
-  for( ; sf < sl; ++sf, --sl, ++rf, --rl) {
+  for( ; sf < sl; ++sf, --sl, ++rf, --rl) { // iterates over sequence and res from both ends towards the middle. For each pair of elements, it computes the complement of the element from sequence and stores it in the corresponding position in res
     *rf = comp(*sl);
     *rl = comp(*sf);
   }
@@ -34,14 +34,14 @@ std::string reverse_complement(const std::string& sequence) {
   return res;
 }
 
-double compare_sketch_pair(const char* p1, const char* p2, unsigned m, unsigned k, unsigned l, bool circular) {
-  const unsigned block = std::max(l, (unsigned)1) * k;
-  unsigned count = 0;
-  if(!circular || l < 2) {
+double compare_sketch_pair(const char* p1, const char* p2, unsigned m, unsigned k, unsigned l, bool circular) { // compares two sketches of DNA sequences and returns a similarity score
+  const unsigned block = std::max(l, (unsigned)1) * k; // computes the size of each sketch in the character arrays
+  unsigned count = 0; // initializes a counter that will keep track of the number of matching sketches
+  if(!circular || l < 2) { 
     for(unsigned i = 0; i < m; ++i, p1 += block, p2 += block)
-      count += memcmp(p1, p2, block) == 0;
+      count += memcmp(p1, p2, block) == 0; // compare the sketches. If two sketches are identical, memcmp returns 0 and the counter is incremented
   } else {
-    for(unsigned i = 0; i < m; ++i, p1 += block, p2 += block) {
+    for(unsigned i = 0; i < m; ++i, p1 += block, p2 += block) { // compares the sketches in a circular manner, meaning that it considers rotations of the sketches as well. If a rotation of a sketch from p1 matches a sketch from p2, the counter is incremented
       for(unsigned j = 0; j < l; ++j) {
         if(memcmp(p1, p2 + j * k, block - j * k) == 0 && memcmp(p1 + block - j * k, p2, j * k) == 0) {
           ++count;
@@ -50,52 +50,52 @@ double compare_sketch_pair(const char* p1, const char* p2, unsigned m, unsigned 
       }
     }
   }
-  return (double)count / m;
+  return (double)count / m; // computes the similarity score as the number of matching sketches divided by the total number of sketches
 }
 
-double compare_sketches(const sketch& sk1, const sketch& sk2, ssize_t m, bool circular) {
+double compare_sketches(const sketch& sk1, const sketch& sk2, ssize_t m, bool circular) { // ssize_t m: This is the number of sketches to compare
   if(sk1.k != sk2.k || sk1.l != sk2.l) return -1; // Different k or l
-  if(m < 0) m = std::min(sk1.m, sk2.m);
+  if(m < 0) m = std::min(sk1.m, sk2.m); 
   if(m > sk1.m || m > sk2.m) return -1;  // Too short
 
   const unsigned block = std::max(sk1.l, (unsigned)1) * sk1.k;
   if(sk1.data.size() < m * block || sk2.data.size() < m * block) return -1; // Truncated
-  const double fwd_score = compare_sketch_pair(sk1.data.data(), sk2.data.data(), m, sk1.k, sk1.l, circular);
+  const double fwd_score = compare_sketch_pair(sk1.data.data(), sk2.data.data(), m, sk1.k, sk1.l, circular); // computes a similarity score for the forward strands of the sequences using the compare_sketch_pair function
 
   double bwd_score = 0.0;
   if(!sk1.rcdata.empty()) {
-    bwd_score = compare_sketch_pair(sk1.rcdata.data(), sk2.data.data(), m, sk1.k, sk1.l, circular);
+    bwd_score = compare_sketch_pair(sk1.rcdata.data(), sk2.data.data(), m, sk1.k, sk1.l, circular); // computes a similarity score for the reverse complements of the sequences, if they are available
   } else if(!sk2.rcdata.empty()) {
-    bwd_score = compare_sketch_pair(sk1.data.data(), sk2.rcdata.data(), m, sk1.k, sk1.l, circular);
+    bwd_score = compare_sketch_pair(sk1.data.data(), sk2.rcdata.data(), m, sk1.k, sk1.l, circular); // If only one sequence has a reverse complement, it compares the forward strand of the other sequence with the reverse complement
   }
 
-  return std::max(fwd_score, bwd_score);
+  return std::max(fwd_score, bwd_score); // the maximum of the forward and reverse complement similarity scores
 }
 
-void sketch::read(std::istream& is) {
-  if(is.get() != '>') throw std::runtime_error("Invalid format, expecting '>'");
+void sketch::read(std::istream& is) { //  reads a sketch from an input stream and stores it in the sketch object; the sketch object contains the sketch read from the input stream
+  if(is.get() != '>') throw std::runtime_error("Invalid format, expecting '>'"); // reads the first character from the input stream and checks if it’s a > character, which is a common convention in bioinformatics file formats. If the first character is not >, it throws an exception
   bool rc;
-  is >> name >> k >> l >> m >> rc;
-  is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  if(!is.good()) throw std::runtime_error("Invalid header");
-  data.resize(std::max(l, (unsigned)1) * m * k);
-  is.read(data.data(), data.size());
+  is >> name >> k >> l >> m >> rc; // reads the name of the sequence and the parameters k, l, m, and rc from the input stream
+  is.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignores the rest of the current line in the input stream, up to and including the newline character
+  if(!is.good()) throw std::runtime_error("Invalid header"); 
+  data.resize(std::max(l, (unsigned)1) * m * k); //  resizes the data vector to hold the sketch data
+  is.read(data.data(), data.size()); // reads the sketch data from the input stream into the data vector
   is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   if(!is.good()) throw std::runtime_error("Error reading sketch");
   if(rc) {
-    rcdata.resize(std::max(l, (unsigned)1) * m * k);
-    is.read(rcdata.data(), rcdata.size());
+    rcdata.resize(std::max(l, (unsigned)1) * m * k); 
+    is.read(rcdata.data(), rcdata.size()); // reads a sketch of the reverse complement of the sequence from the input stream into the rcdata vector
     is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     if(!is.good()) throw std::runtime_error("Error reading rc sketch");
   }
 }
 
-void sketch::write(std::ostream& os) const {
-  os << '>' << name << ' ' << k << ' ' << l << ' ' << m << ' ' << !rcdata.empty() << '\n';
-  os.write(data.data(), data.size());
+void sketch::write(std::ostream& os) const { // writes a sketch to an output stream
+  os << '>' << name << ' ' << k << ' ' << l << ' ' << m << ' ' << !rcdata.empty() << '\n'; // writes the name of the sequence and the parameters k, l, m, and a boolean indicating whether rcdata is not empty (which would mean that the sketch includes the reverse complement of the sequence) to the output stream
+  os.write(data.data(), data.size()); // writes the sketch data to the output stream
   os << '\n';
   if(!rcdata.empty()) {
-    os.write(rcdata.data(), rcdata.size());
+    os.write(rcdata.data(), rcdata.size()); // writes the sketch data for the reverse complement of the sequence to the output stream
     os << '\n';
   }
 }
--- omh_compute/omh.hpp
+++ omh_compute/omh.hpp
@@ -15,10 +15,12 @@
 std::string reverse_complement(const std::string&);
 
 struct mer_info {
-  size_t pos;
-  uint64_t hash;
-  unsigned occ;
-  mer_info(size_t p, unsigned o, uint64_t h)
+  """ mer_info class, initialize by mer_info(p, o, h)
+  """
+  size_t pos; // position in the sequence
+  uint64_t hash; // store the hashing value
+  unsigned occ; // store the number of occurences
+  mer_info(size_t p, unsigned o, uint64_t h) // constructor
     : pos(p)
     , hash(h)
     , occ(o)
@@ -35,54 +37,54 @@ void omh_pos(const std::string& seq, unsigned k, unsigned l, unsigned m, EngineT
   if(l == 0) l = 1;
 
   std::vector<mer_info> mers;
-  std::unordered_map<std::string, unsigned> occurrences;
+  std::unordered_map<std::string, unsigned> occurrences; // default dictionary: counting occurence of k-mer
   size_t pos[l];
 
   //  Create list of k-mers with occurrence numbers
   for(size_t i = 0; i < seq.size() - k + 1; ++i) {
     auto occ = occurrences[seq.substr(i, k)]++;
-    mers.emplace_back(i, occ, (uint64_t)0);
+    mers.emplace_back(i, occ, (uint64_t)0); // (pos_in_seq, occ, hash)
   }
 
-  xxhash hash;
+  xxhash hash; // creates an instance of the xxhash class, which is used to compute hash values
   for(unsigned i = 0; i < m; ++i) {
-    const auto seed = prg();
+    const auto seed = prg(); // prg: pseudo random generator
     for(auto& meri : mers) {
-      hash.reset(seed);
-      hash.update(&seq.data()[meri.pos], k);
-      if(weight) hash.update(&meri.occ, sizeof(meri.occ));
-      meri.hash = hash.digest();
+      hash.reset(seed); // resets the xxhash object with the new seed
+      hash.update(&seq.data()[meri.pos], k); // updates the hash value with the k-mer starting at position meri.pos in the sequence. The seq.data() function returns a pointer to the sequence string, and &seq.data()[meri.pos] gives a pointer to the start of the k-mer.
+      if(weight) hash.update(&meri.occ, sizeof(meri.occ)); // updates the hash value with the number of occurrences of the k-mer. The sizeof(meri.occ) function returns the size of the occ member in bytes.
+      meri.hash = hash.digest(); // computes the final hash value and stores it in the hash member of the mer_info object
     }
 
-    std::partial_sort(mers.begin(), mers.begin() + l, mers.end(), [&](const mer_info& x, const mer_info& y) { return x.hash < y.hash; });
-    std::sort(mers.begin(), mers.begin() + l, [&](const mer_info& x, const mer_info& y) { return x.pos < y.pos; });
+    std::partial_sort(mers.begin(), mers.begin() + l, mers.end(), [&](const mer_info& x, const mer_info& y) { return x.hash < y.hash; }); // partially sorts the mers vector. The elements from mers.begin() to mers.begin() + l are sorted in ascending order based on their hash values. The rest of the vector is not sorted.
+    std::sort(mers.begin(), mers.begin() + l, [&](const mer_info& x, const mer_info& y) { return x.pos < y.pos; }); // sorts the first l elements of the mers vector in ascending order based on their positions in the sequence.  the first l elements of mers are the l k-mers with the smallest hash values, and they are ordered by their positions in the sequence.
     for(unsigned j = 0; j < l; ++j)
       block(i, j, mers[j].pos);
   }
 }
 
-struct sketch {
-  std::string       name;
-  unsigned          k, l, m;
-  std::vector<char> data;
-  std::vector<char> rcdata;
+struct sketch { // a data structure that represents a sketch of a DNA sequence
+  std::string       name; // holds the name of the sequence
+  unsigned          k, l, m; // 
+  std::vector<char> data; // a vector of characters that likely holds the sketch data for the original sequence.
+  std::vector<char> rcdata; //  a vector of characters that likely holds the sketch data for the reverse complement of the sequence
 
-  bool operator==(const sketch& rhs) const {
+  bool operator==(const sketch& rhs) const { // an equality operator that checks if two sketches are equal. Two sketches are considered equal if their k, l, m, data, and rcdata members are all equal
     return k == rhs.k && l == rhs.l && m == rhs.m && data == rhs.data && rcdata == rhs.rcdata;
   }
 
   // Read a sketch into this
-  void read(std::istream& is);
+  void read(std::istream& is); // reads a sketch from an input stream and stores it in the sketch object
 
   // Read a sketch
-  static sketch from_stream(std::istream& is) {
+  static sketch from_stream(std::istream& is) { // creates a new sketch object, reads a sketch from an input stream into it, and returns it
     sketch sk;
     sk.read(is);
     return sk;
   }
 
   // Write sketch
-  void write(std::ostream& os) const;
+  void write(std::ostream& os) const; //  writes the sketch to an output stream.
 };
 
 template<typename EngineT = std::mt19937_64>
@@ -93,12 +95,12 @@ protected:
   EngineT            m_prg;
 
   inline void write_sketch(std::ostream& os, const std::string& seq) {
-    omh_pos(seq, m_k, m_l, m_m, m_prg, [&os, &seq, this](unsigned i, unsigned j, size_t pos) { os.write(seq.data() + pos, m_k); });
+    omh_pos(seq, m_k, m_l, m_m, m_prg, [&os, &seq, this](unsigned i, unsigned j, size_t pos) { os.write(seq.data() + pos, m_k); }); // writes the k-mer starting at position pos in the sequence to the output stream
   }
 
   inline void compute_sketch(char* ptr, const char* seq) {
     omh_pos(seq, m_k, m_l, m_m, m_prg,
-            [&ptr, &seq, this](unsigned i, unsigned j, size_t pos) { memcpy(ptr, seq + pos, m_k); ptr += m_k; });
+            [&ptr, &seq, this](unsigned i, unsigned j, size_t pos) { memcpy(ptr, seq + pos, m_k); ptr += m_k; }); //  the character array pointed to by ptr contains the sketches of all k-mers in the sequence.
   }
 
 public:
@@ -107,21 +109,21 @@ public:
     , m_seed(seed)
   { }
 
-  // Compute the positions in seq of the k-mers picked by omh and append then to res
+  // Compute the positions in seq of the k-mers picked by omh and append them to res
   void pos(const std::string& seq, std::vector<size_t>& res) {
     m_seed(m_prg);
-    omh_pos(seq, m_k, m_l, m_m, m_prg, [&res](unsigned i, unsigned j, size_t pos) { res.push_back(pos); });
+    omh_pos(seq, m_k, m_l, m_m, m_prg, [&res](unsigned i, unsigned j, size_t pos) { res.push_back(pos); }); // res vector contains the positions of all k-mers in the sequence that were selected by the omh_pos function
   }
 
   // Compute the positions in seq and return vector
   inline std::vector<size_t> pos(const std::string& seq) {
     std::vector<size_t> res;
-    pos(seq, res);
+    pos(seq, res); // call pos above
     return res;
   }
 
   // Write sketch to an ostream
-  void write(std::ostream& os, const std::string& name, const std::string& seq, bool rc = false) {
+  void write(std::ostream& os, const std::string& name, const std::string& seq, bool rc = false) { // os contains a sketch of the sequence and possibly its reverse complement, along with some metadata
     m_seed(m_prg);
     os << '>' << name << ' ' << m_k << ' ' << m_l << ' ' << m_m << ' ' << rc << '\n';
     write_sketch(os, seq);
@@ -141,7 +143,7 @@ public:
     sk.m = m_m;
     sk.data.resize(std::max(sk.l, (unsigned)1) * sk.m * sk.k);
     m_seed(m_prg);
-    compute_sketch(sk.data.data(), seq.data());
+    compute_sketch(sk.data.data(), seq.data()); // computes a sketch of a DNA sequence and stores it in a sketch object. The sketch object is updated with the parameters k, l, m, and the sketch data.
 
     if(rc) {
       m_seed(m_prg);
@@ -160,8 +162,8 @@ public:
 
   double compare(const std::string& s1, const std::string& s2, bool rc = false) {
     const auto sk1 = compute(s1, rc);
-    const auto sk2 = compute(s2, rc);
-    return compare_sketches(sk1, sk2);
+    const auto sk2 = compute(s2, rc); // computes sketches of two DNA sequences (and their reverse complements if rc is true)
+    return compare_sketches(sk1, sk2); // returns a double value that represents the similarity between the two sequences
   }
 };
 
